ec2452dc12371d2547fc1f212205b2a9cf26ecb3
huahandsome <huahandsome@gmail.com>
2017-05-05 23:18:37 -0700

    migrate sale transaction data from mongo to mysql

--- src/main/java/com/searcher/model/entity/SaleTransactionBean.java
+++ src/main/java/com/searcher/model/entity/SaleTransactionBean.java
@@ -8,4 +8,44 @@ import lombok.Data;
 
 @Data
 public class SaleTransactionBean {
+    private int saleTransactionId;
+    private int quantity;
+    private int time;
+    private int medicineId;
+    private int storeId;
+    private int customerId;
+    private int factoryId;
+    private double totalPrice;
+
+    public int getSaleTransactionId()  {
+        return saleTransactionId;
+    }
+
+    public int getQuantity()  {
+        return quantity;
+    }
+
+    public int getTime()  {
+        return time;
+    }
+
+    public int getMedicineId()  {
+        return medicineId;
+    }
+
+    public int getStoreId()  {
+        return storeId;
+    }
+
+    public int getCustomerId()  {
+        return customerId;
+    }
+
+    public int getFactoryId()  {
+        return factoryId;
+    }
+
+    public double getTotalPrice()  {
+        return totalPrice;
+    }
 }
--- src/main/java/com/searcher/utils/MysqlUtils.java
+++ src/main/java/com/searcher/utils/MysqlUtils.java
@@ -9,12 +9,73 @@ import java.util.ArrayList;
  * Created by ss on 2017/5/3.
  */
 public class MysqlUtils {
+    private static final String URL      = "jdbc:mysql://localhost:3306/226operation?serverTimezone=GMT";
+    private static final String USERNAME = "ultimate";
+    private static final String PASSWORD = "sesame";
+    private static final String saleTransactionTbl = "saleTransaction";
 
     static public void persistSaleTransaction(ArrayList<SaleTransactionBean> saleTransactionBeans) {
+        createSaleTransactionTable();
 
+        for ( int idx = 0; idx < saleTransactionBeans.size(); idx++ ) {
+            SaleTransactionBean saleTransaction = saleTransactionBeans.get(idx);
+            insertIntoSaleTransactionTbl(saleTransaction);
+        }
     }
 
     static public void persistSearchTransaction(ArrayList<SearchTransactionBean> searchTransactionBeans) {
 
+    }
+
+    private void createSaleTransactionTable(){
+        String sql = "CREATE TABLE IF NOT EXISTS saleTransaction ( " +
+                     "saleTransactionKey    INT     NOT NULL," +
+                     "quantity              INT     NOT NULL," +
+                     "calendarKey           INT     NOT NULL," +
+                     "medicineKey           INT     NOT NULL," +
+                     "storeKey              INT     NOT NULL," +
+                     "customerKey           INT     NOT NULL," +
+                     "factoryKey            INT     NOT NULL," +
+                     "totalPrice            DOUBLE  NOT NULL," +
+                     "PRIMARY KEY (saleTransactionKey) ";
+
+        try {
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Statement stmt  = c.createStatement();
+            stmt.execute( sql );
+
+            stmt.close();
+            conn.close();
+        }catch ( Exception e ) {
+            e.printStackTrace();
+        }
+    }
+
+    private void insertIntoSaleTransactionTbl(SaleTransactionBean saleTransaction) {
+        int saleTransKey  = saleTransaction.getSaleTransactionId();
+        int quantity      = saleTransaction.getQuantity();
+        int calendarKey   = saleTransaction.getTime();// ??
+        int medicineKey   = saleTransaction.getMedicineId();
+        int storeKey      = saleTransaction.getStoreId();
+        int customerKey   = saleTransaction.getCustomerId();
+        int factoryKey    = saleTransaction.getFactoryId();
+        double totalPrice = saleTransaction.getTotalPrice();
+
+        String sql = String.format( "INSERT INTO saleTransaction " +
+                                    "(saleTransactionKey, quantity, calendarKey, medicineKey, storeKey, customerKey, factoryKey, totalPrice)" +
+                                    " VALUES " +
+                                    "(%d, %d, %d, %d, %d, %d, %d, %f)",
+                                    saleTransKey, quantity, calendarKey, medicineKey, storeKey, customerKey, factoryKey, totalPrice );
+
+        try {
+            Connection conn   = DriverManager.getConnection( URL, USERNAME, PASSWORD );    //connect to mysql
+            Statement stmt    = conn.createStatement();
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+            conn.close();
+        }catch( Exception e ){
+            e.printStackTrace();
+        }
     }
 }
