90e7b8723af7d784982fc10c267e2ebf3a11d1cc
huahandsome <huahandsome@gmail.com>
2017-06-07 15:02:38 -0700

    modify ELT to update nine fact tables

--- src/main/java/com/searcher/model/entity/SaleTransactionBean.java
+++ src/main/java/com/searcher/model/entity/SaleTransactionBean.java
@@ -29,14 +29,13 @@ public class SaleTransactionBean {
     //public int getSaleTransactionId()  {
     //    return _id;
     //}
-    //public SaleTransactionBean( int q, long t, int mId, int sId, int cId, double p ) {
-    //    quantity   = q;
-    //    time       = t;
-    //    medicineId = mId;
-    //    storeId    = sId;
-    //    customerId = cId;
-    //    totalPrice = p;
-    //}
+    public SaleTransactionBean( long t, int mId, int sId, int cId, double p ) {
+        time       = t;
+        medicineId = mId;
+        storeId    = sId;
+        customerId = cId;
+        totalPrice = p;
+    }
 
     //public int getQuantity()  {
     //    return quantity;
